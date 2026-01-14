9bcc92b0921b8e7fcb31699e2e01c1d6eed8087e
Teta <tetref@users.noreply.github.com>
2026-01-14 20:38:30 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/yieldnode/main.go
@@ -0,0 +1,23 @@
+// cmd/yieldnode/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "yieldnode/internal/yieldnode"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := yieldnode.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
